5b9e9f3704b3fa6ada32e822371bdf427ab706b8
yasuo9528 <2014735959@qq.com>
2018-09-18 17:07:57 +0800

    111

--- src/components/BlogHeader.vue
+++ src/components/BlogHeader.vue
@@ -2,8 +2,7 @@
 	<el-menu :default-active="$route.path" class="el-menu-demo" mode="horizontal"
 	background-color="#545c64" text-color="#fff" active-text-color="#ffd04b" router>
 		<el-menu-item index="/">博客</el-menu-item>
-		<el-menu-item index="/add">写博客</el-menu-item>
-		
+		<el-menu-item index="/add">写博客</el-menu-item>	
 	</el-menu>
 </template>
 
